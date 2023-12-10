fe5cc78a429249be1b2d980e1b097eba439503d5
tanzim_safin <tanzimhossainsafin756@gmail.com>
2023-12-10 18:52:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+$("nav").addClass("h");
+$(".btn").css("color","black").html("<em>Search</em>");
+$("button.btn").css("background-color","rgb(255, 21, 0)");
+$(".moto text").animate("opacity:0.5");
+$(".hospital_pic").css("border-radius","20%")
